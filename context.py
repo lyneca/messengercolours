4762d58a1c7083ecc1a8d1d5ad59a8f651ce10c8
Luke Tuthill <lukegithub@gmail.com>
2016-03-18 00:21:23 +1100

    push for test purposes

--- context.py
+++ context.py
@@ -1,1 +1,4 @@
 # paste in your Python cURL request here: http://curl.trillworks.com/
+headers = {
+    'cookie': 'datr=5f6uVqVsvp1VLKVMSvJ6kxr4; p=-2; __lfcc=1; c_user=100009563164658; xs=127%3AQNC_6NFGhIcvYg%3A2%3A1454309099%3A2850; csm=2; s=Aa7-vIslzkDs2ekP; lu=gAPcQVOYEHhGwZ_e6QWCBKgw; __lncc_l.facebook.com=1; presence=EDvF3EtimeF1458219347EuserFA21B09563164658A2EstateFDutF1458219347571CEchFDp_5f1B09563164658F2325CC; act=1458219372418%2F97',
+}
